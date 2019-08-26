487989be6877a7348cd007f1ba54150575dd657a
Tseen She <ts33n.sh3@gmail.com>
2019-08-26 21:06:50 +0100

    skeleton for contrib-stack

---
+++ haskell-tng-contrib-stack.el
@@ -0,0 +1,16 @@
+;;; haskell-tng-contrib-stack.el --- sets stack as the default -*- lexical-binding: t -*-
+
+;; Copyright (C) 2019 Tseen She
+;; License: GPL 3 or any later version
+
+;;; Commentary:
+;;
+;;  Overrides the defaults with stack variants.
+;;
+;;; Code:
+
+;; TODO document
+;; FIXME implement
+
+(provide 'haskell-tng-contrib-stack)
+;;; haskell-tng-contrib-stack.el ends here
