330330afe581d95e9a6fd2eb199bb7ae9ae16392
Jeronimo Pellegrini <j_p@aleph0.info>
2021-05-02 08:25:03 -0300

    Add tests on both (STklos and Emacs) sides.

--- Makefile
+++ Makefile
@@ -2,5 +2,14 @@ all:
 	@echo "There is no need to call make ot install Geiser-STklos."
 	@echo "You can call 'make test' to test it, though."
 
-test:
+.PHONY: test-stklos
+test-stklos:
 	stklos --no-init-file --utf8-encoding=yes -f geiser-stklos-test.stk
+
+.PHONY: test-emacs
+test-emacs:
+	@emacs -batch -l ert -l geiser-stklos-test.el -f ert-run-tests-batch-and-exit 1> test-emacs-stdout.log
+
+test: test-stklos test-emacs
+
+
---
+++ geiser-stklos-test.el
@@ -0,0 +1,34 @@
+
+(setq load-path (cons "../geiser/elisp" (cons "." load-path)))
+(require 'geiser)
+(load "geiser-stklos.el")
+;(eval-when-compile
+(require 'cl-lib)
+;)
+
+
+(ert-deftest geiser-stklos--loaded ()
+  (should (member 'geiser-stklos features)))
+
+(ert-deftest find-close-par ()
+  29
+  (with-temp-buffer
+    (insert "(let* ((let 'let) (let* let)) let)")
+    (goto-char 7)
+    (find-close-par)))
+
+(ert-deftest find-close-par-2 ()
+  18
+  (with-temp-buffer
+    (insert "(let* ((let 'let) (let* let)) let)")
+    (goto-char 10)
+    (find-close-par 8)))
+
+
+;; this one could have many more variants
+(ert-deftest geiser-stklos--symbol-begin ()
+  5
+  (with-temp-buffer
+    (insert "(abc def (ghijkl))")
+    (goto-char 7)
+    (geiser-stklos--symbol-begin nil)))
