b53c3d8caa8f33a17efe522e83a62aecf6689d5c
Daniel Semyonov <daniel@dsemy.com>
2021-06-01 01:08:43 +0300

    ; Fix case in license part of vcomplete.el

--- vcomplete.el
+++ vcomplete.el
@@ -11,12 +11,12 @@
 
 ;; This file is not part of GNU Emacs.
 
-;; vcomplete is free software; you can redistribute it and/or modify
+;; Vcomplete is free software; you can redistribute it and/or modify
 ;; it under the terms of the GNU General Public License as published
 ;; by the Free Software Foundation; either version 3, or (at your
 ;; option) any later version.
 
-;; vcomplete is distributed in the hope that it will be useful, but
+;; Vcomplete is distributed in the hope that it will be useful, but
 ;; WITHOUT ANY WARRANTY; without even the implied warranty of
 ;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
 ;; General Public License for more details.
