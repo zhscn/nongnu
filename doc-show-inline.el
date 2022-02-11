5705f8d6f5583d50fc0497a88f06fb7c84a9ddb0
Campbell Barton <ideasman42@gmail.com>
2022-02-11 16:48:07 +1100

    Cleanup: use brief SPDX license

--- doc-show-inline.el
+++ doc-show-inline.el
@@ -1,5 +1,6 @@
 ;;; doc-show-inline.el --- Show doc-strings found in external files -*- lexical-binding: t -*-
 
+;; SPDX-License-Identifier: GPL-2.0-or-later
 ;; Copyright (C) 2021  Campbell Barton
 
 ;; Author: Campbell Barton <ideasman42@gmail.com>
@@ -8,19 +9,6 @@
 ;; Keywords: convenience
 ;; Version: 0.1
 ;; Package-Requires: ((emacs "26.2"))
-
-;; This program is free software; you can redistribute it and/or modify
-;; it under the terms of the GNU General Public License as published by
-;; the Free Software Foundation, either version 3 of the License, or
-;; (at your option) any later version.
-
-;; This program is distributed in the hope that it will be useful,
-;; but WITHOUT ANY WARRANTY; without even the implied warranty of
-;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-;; GNU General Public License for more details.
-
-;; You should have received a copy of the GNU General Public License
-;; along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
 ;;; Commentary:
 
@@ -444,6 +432,7 @@ the point should not be moved by this function."
   "Return a list of XREF items from the identifier SYM at the current point.
 
 Argument XREF-BACKEND is used to avoid multiple calls to `xref-find-backend'."
+  ;; (printf "SYM: %S\n" sym)
   (let ((xref-list nil))
     (doc-show-inline--with-advice 'xref--not-found-error
       :override (lambda (_kind _input) nil)
