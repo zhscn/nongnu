39b39a7265c59e413ff32f1fccebe4dc1a7a966e
Campbell Barton <ideasman42@gmail.com>
2022-03-23 12:06:43 +1100

    Don't specify a theme
    The themes may change over time,
    be more future proof and use the default theme.

--- diff-ansi.el
+++ diff-ansi.el
@@ -57,7 +57,7 @@ This must take the diff content as the `standard-input'."
 ;; Extra arguments for command presets.
 
 (defcustom diff-ansi-extra-args-for-delta
-  (list "--side-by-side" "--no-gitconfig" "--syntax-theme=github" "--true-color=always")
+  (list "--side-by-side" "--no-gitconfig" "--true-color=always")
   "Additional arguments to pass to `delta'."
   :type '(repeat string))
 
