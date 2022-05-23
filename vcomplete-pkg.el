a64c37e78011e0c5bb52085ae69e1e150550500d
Daniel Semyonov <daniel@dsemy.com>
2022-05-23 13:59:41 +0300

    ; Generate 'vcomplete-pkg.el' automatically

--- vcomplete-pkg.el
+++
@@ -1,9 +0,0 @@
-(define-package "vcomplete" "1.2" "Visual completions"
-  '((emacs "25.1"))
-  :keywords ("convenience" "matching")
-  :authors (("Daniel Semyonov" . "daniel@dsemy.com"))
-  :maintainer ("Daniel Semyonov" . "daniel@dsemy.com")
-  :url "https://dsemy.com/projects/vcomplete")
-;; Local Variables:
-;; no-byte-compile: t
-;; end:
