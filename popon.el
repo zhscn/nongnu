b5b370077111ae65f50f8a3dca76e7c270ff0743
Akib Azmain Turja <akib@disroot.org>
2022-07-28 00:06:42 +0600

    Bump version to 0.9

--- popon.el
+++ popon.el
@@ -4,7 +4,7 @@
 
 ;; Author: Akib Azmain Turja <akib@disroot.org>
 ;; Created: 2022-04-11
-;; Version: 0.8
+;; Version: 0.9
 ;; Package-Requires: ((emacs "25.1"))
 ;; Keywords: lisp extensions frames
 ;; Homepage: https://codeberg.org/akib/emacs-popon
