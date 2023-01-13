f10837099d48454c470aa04a3803a8903d421d23
Campbell Barton <ideasman42@gmail.com>
2023-01-13 23:54:25 +1100

    Fix compatibility for pos-bol/eol for emacs <29.1

--- diff-ansi.el
+++ diff-ansi.el
@@ -21,6 +21,19 @@
 ;;; Code:
 (require 'ansi-color)
 
+
+;; ---------------------------------------------------------------------------
+;; Compatibility
+
+(when (version< emacs-version "29.1")
+  (defsubst pos-bol (&optional n)
+    "Return the position at the line beginning."
+    (line-beginning-position n))
+  (defsubst pos-eol (&optional n)
+    "Return the position at the line end."
+    (line-end-position n)))
+
+
 ;; ---------------------------------------------------------------------------
 ;; Custom Variables
 
