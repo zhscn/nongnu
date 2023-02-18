45e00b20b120784ad1c857a4fb71f994d17ef6c7
marty hiatt <martianhiatus [a t] riseup [d o t] net>
2023-02-18 22:07:17 +0100

    handle no "next" link header, roughly
    comment typo
    
    fix --more re link-header

--- lisp/mastodon-tl.el
+++ lisp/mastodon-tl.el
@@ -2767,16 +2767,21 @@ when showing followers or accounts followed."
   (message "Loading older toots...")
   (if (mastodon-tl--use-link-header-p)
       ;; link-header: can't build a URL with --more-json-async, endpoint/id:
-      (let* ((next (car (mastodon-tl--link-header)))
-             ;;(prev (cadr (mastodon-tl--link-header)))
-             (url (mastodon-tl--build-link-header-url next)))
-        (mastodon-http--get-response-async url nil 'mastodon-tl--more* (current-buffer)
-                                           (point) :headers))
-    (mastodon-tl--more-json-async
-     (mastodon-tl--get-endpoint)
-     (mastodon-tl--oldest-id)
-     (mastodon-tl--update-params)
-     'mastodon-tl--more* (current-buffer) (point))))
+      ;; ensure we have a "next" type here, otherwise the CAR will be the
+      ;; "prev" type!
+      (let ((link-header (mastodon-tl--link-header)))
+        (if (> 2 (length link-header))
+            (error "No next page")
+          (let* ((next (car link-header))
+                 ;;(prev (cadr (mastodon-tl--link-header)))
+                 (url (mastodon-tl--build-link-header-url next)))
+            (mastodon-http--get-response-async url nil 'mastodon-tl--more* (current-buffer)
+                                               (point) :headers))
+          (mastodon-tl--more-json-async
+           (mastodon-tl--get-endpoint)
+           (mastodon-tl--oldest-id)
+           (mastodon-tl--update-params)
+           'mastodon-tl--more* (current-buffer) (point))))))
 
 (defun mastodon-tl--more* (response buffer point-before &optional headers)
   "Append older toots to timeline, asynchronously.
