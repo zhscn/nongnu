619ebc6cb1f65280f1be785b92ce699575732c74
marty hiatt <martianhiatus [a t] riseup [d o t] net>
2023-02-28 21:31:19 +0100

    insert spaces before argument lists for lambda

--- lisp/mastodon-toot.el
+++ lisp/mastodon-toot.el
@@ -902,8 +902,8 @@ Buffer-local variable `mastodon-toot-previous-window-config' holds the config."
 (defun mastodon-toot--mentions-to-string (mentions)
   "Apply `mastodon-toot--process-local' function to each mention in MENTIONS.
 Remove empty string (self) from result and joins the sequence with whitespace."
-  (mapconcat (lambda(mention) mention)
-	     (remove "" (mapcar (lambda(x) (mastodon-toot--process-local x))
+  (mapconcat (lambda (mention) mention)
+	     (remove "" (mapcar (lambda (x) (mastodon-toot--process-local x))
 				mentions))
 	     " "))
 
