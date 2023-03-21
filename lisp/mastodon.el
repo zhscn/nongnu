09e3f9fe6eb8d94bc5f44ac5c094479489c4929e
marty hiatt <martianhiatus [a t] riseup [d o t] net>
2023-03-21 16:16:48 +0100

    bind switch buffers to "/"

--- lisp/mastodon.el
+++ lisp/mastodon.el
@@ -162,6 +162,7 @@ Use. e.g. \"%c\" for your locale's date and time format."
     (define-key map (kbd "@") #'mastodon-notifications--get-mentions)
     (define-key map (kbd "P") #'mastodon-profile--show-user)
     (define-key map (kbd "s") #'mastodon-search--search-query)
+    (define-key map (kbd "/") #'mastodon-switch-to-buffer)
     ;; quitting mastodon
     (define-key map (kbd "q") #'kill-current-buffer)
     (define-key map (kbd "Q") #'kill-buffer-and-window)
