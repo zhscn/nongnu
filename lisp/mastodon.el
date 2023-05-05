a411879809215d09520e3a7c21d3f07a3f2b5272
marty hiatt <martianhiatus [a t] riseup [d o t] net>
2023-05-05 16:59:29 +0200

    wrap commentary

--- lisp/mastodon.el
+++ lisp/mastodon.el
@@ -29,8 +29,11 @@
 
 ;;; Commentary:
 
-;; mastodon.el is a client for fediverse services that implement the Mastodon API. See <https://github.com/mastodon/mastodon>.
-;; See the readme file at https://codeberg.org/martianh/mastodon.el for set up and usage details.
+;; mastodon.el is a client for fediverse services that implement the Mastodon
+;; API. See <https://github.com/mastodon/mastodon>.
+
+;; See the readme file at https://codeberg.org/martianh/mastodon.el for set up
+;; and usage details.
 
 ;;; Code:
 (require 'cl-lib) ; for `cl-some' call in mastodon
