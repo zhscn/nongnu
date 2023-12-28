a61f9c27b1ab00dc4582c363568482c25d677a2f
Vivek Das Mohapatra <vivek@collabora.co.uk>
2023-12-28 23:08:16 +0000

    Remove unnecessary file

--- totp-pkg.el
+++
@@ -1,6 +0,0 @@
-(define-package "totp" "0.1"
-  "A Time-based (RFC6238) One Time Password (TOTP) generator."
-  '((emacs "27.1"))
-  :authors '(("Vivek Das Mohapatra" . "vivek@etla.org"))
-  :keywords '("otp" "totp" "two-factor" "password" "2fa")
-  :url "https://gitlab.com/fledermaus/totp.el")
