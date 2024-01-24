7e7e43535e0bd9fdf5d373780a47ee622526b9a3
Vivek Das Mohapatra <vivek@collabora.com>
2024-01-24 21:06:00 +0000

    Update references to old command names in the commentary section

--- totp-auth.el
+++ totp-auth.el
@@ -20,9 +20,9 @@
 ;; with ‘auth-sources’ and/or the freedesktop secrets API (aka
 ;; Gnome Keyring or KWallet).
 ;;
-;; You can call it with the command ‘totp’, ie:
+;; You can call it with the command ‘totp-auth’, ie:
 ;;
-;;    M-x totp RET
+;;    M-x totp-auth RET
 ;;
 ;; You can tab-complete based on the label of the secret.
 ;; Depending on the setting of ‘totp-auth-display-token-method’ the
