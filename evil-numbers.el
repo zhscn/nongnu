b6fcd2e23f6091eb04596aeb19b102d1790e83a7
Campbell Barton <ideasman42@gmail.com>
2024-04-16 11:40:11 +1000

    Cleanup: use a single comment block for "Commentary"

--- evil-numbers.el
+++ evil-numbers.el
@@ -18,26 +18,26 @@
 ;;; Commentary:
 
 ;; Increment / Decrement binary, octal, decimal and hex literals.
-
+;;
 ;; Works like C-a/C-x in VIM, i.e. searches for number up to EOL and
 ;; then increments or decrements and keep zero padding up.
-
+;;
 ;; Known Bugs:
 ;; See http://github.com/juliapath/evil-numbers/issues
-
+;;
 ;; Install:
-
+;;
 ;; (require 'evil-numbers)
-
+;;
 ;; and bind, for example:
-
+;;
 ;; (global-set-key (kbd "C-c +") 'evil-numbers/inc-at-pt)
 ;; (global-set-key (kbd "C-c -") 'evil-numbers/dec-at-pt)
 ;; (global-set-key (kbd "C-c C-+") 'evil-numbers/inc-at-pt-incremental)
 ;; (global-set-key (kbd "C-c C--") 'evil-numbers/dec-at-pt-incremental)
-
+;;
 ;; or only in evil's normal and visual state:
-
+;;
 ;; (evil-define-key '(normal visual) 'global (kbd "C-c +")
 ;;                  'evil-numbers/inc-at-pt)
 ;; (evil-define-key '(normal visual) 'global (kbd "C-c -")
@@ -46,7 +46,7 @@
 ;;                  'evil-numbers/inc-at-pt-incremental)
 ;; (evil-define-key '(normal visual) 'global (kbd "C-c C--")
 ;;                  'evil-numbers/dec-at-pt-incremental)
-
+;;
 ;; Usage:
 ;; Go and play with your numbers!
 
