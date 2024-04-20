e752d84a9cb6371b147cbaee5da7939ccd900480
Campbell Barton <ideasman42@gmail.com>
2024-04-20 21:00:34 +1000

    Bump emacs version 29.1

--- scroll-on-drag.el
+++ scroll-on-drag.el
@@ -7,7 +7,7 @@
 
 ;; URL: https://codeberg.org/ideasman42/emacs-scroll-on-drag
 ;; Version: 0.1
-;; Package-Requires: ((emacs "26.2"))
+;; Package-Requires: ((emacs "29.1"))
 
 ;;; Commentary:
 
@@ -19,22 +19,6 @@
 ;;
 
 ;;; Code:
-
-
-;; ---------------------------------------------------------------------------
-;; Compatibility
-
-(when (and (version< emacs-version "29.1") (not (and (fboundp 'pos-bol) (fboundp 'pos-eol))))
-  (defun pos-bol (&optional n)
-    "Return the position at the line beginning."
-    (declare (side-effect-free t))
-    (let ((inhibit-field-text-motion t))
-      (line-beginning-position n)))
-  (defun pos-eol (&optional n)
-    "Return the position at the line end."
-    (declare (side-effect-free t))
-    (let ((inhibit-field-text-motion t))
-      (line-end-position n))))
 
 
 ;; ---------------------------------------------------------------------------
